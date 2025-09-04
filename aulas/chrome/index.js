efaf1a44598b0354e7b57efb3b4c56edb8712c60
Victorsoares-vs <victortrexbk@gmail.com>
2025-09-04 16:40:10 -0300

    trabalhando listas no js

--- aulas/chrome/index.js
+++ aulas/chrome/index.js
@@ -1,11 +1,18 @@
 const inputBtn = document.getElementById("input-btn")
+let myLeads = ["aaaaa", "bbbbbbb", "ccccccccc"]
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")
+for(i=0; i < myLeads.length; i++) {
+    ulEl.innerHTML += ( "<li>" + myLeads[i] + "</li>") //se usamos somente "inner text", ele não gera elementos html, só string simples.
+}
+
 
 
     
