20d8ecf40309da41cb46afe96d25bc0d75f97cb3
Victorsoares-vs <victortrexbk@gmail.com>
2025-09-07 23:31:52 -0300

    Update index.js

--- aulas/chrome/index.js
+++ aulas/chrome/index.js
@@ -3,6 +3,11 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+//localStorage.setItem("myName", "Victor Soares")
+let greeting = localStorage.getItem("myName")
+console.log(greeting)
+localStorage.clear()
+
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
