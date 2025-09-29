d09a8746bd3bd7f7fc5d71a65cc372cfda0e0578
Victorsoares-vs <victortrexbk@gmail.com>
2025-09-29 01:05:46 -0300

    parameters in functions

--- aulas/study10/exercise12.js
+++ aulas/study10/exercise12.js
@@ -6,5 +6,13 @@ const sender = "Victor"
 const email = `Hey, ${recipient},
 how is it going? Cheers,
 ${sender}`
-console.log(email)
+//console.log(email)
 
+//EXERCISE 12 - PRACTICING TRUTHY OR FAlSIE VALUES 
+
+console.log(  Boolean("")   ) // false
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true - 0 is false, but array is true.
+console.log(  Boolean(-0)   ) // false
